3ea869e865e54d0b3b3ec6be7e208f7943dcefc6
Jordan Burger <jordanburger22@gmail.com>
2024-07-16 02:05:18 +0000

    added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
 import RandomMovie from "./components/RandomMovie"
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
-  
+  const [theme, setTheme] = useState('light')
+
+  function toggleTheme(){
+    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')
+  }
 
   return (
-    <>
+    <div className={`${theme} main`}>
+      <Navbar toggleTheme={toggleTheme}/>
       <RandomMovie />
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Navbar(props){
+
+    const {toggleTheme} = props
+
+    return(
+        <nav className="nav">
+            <h1>MovieBuddy</h1>
+            <button onClick={toggleTheme}>Toggle Theme</button>
+        </nav>
+    )
+}
