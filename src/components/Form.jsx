ae280a4a1ce756bed357fbfc996a2552c12f6ab4
Jordan Burger <jordanburger22@gmail.com>
2024-07-23 21:44:28 +0000

    added handle change

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Form(props) {
+
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        rating: ''
+    })
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+
     return (
         <form>
-            <input placeholder = "title" />
-            <input placeholder = "description" />
-            <input placeholder = "rating" />
+            <input
+                placeholder="title"
+                onChange={handleChange}
+                name="title"
+                value={formData.title}
+            />
+            <input
+                placeholder="description"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="rating"
+                name="rating"
+                value={formData.rating}
+                onChange={handleChange}
+            />
         </form>
     )
 }
